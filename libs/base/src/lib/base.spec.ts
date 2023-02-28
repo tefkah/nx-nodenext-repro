1384b540daae1f4874683a502f89af5bc452a2ef
CodeSandbox <support@codesandbox.io>
2023-02-28 22:33:44 +0000

    Initial commit

---
+++ libs/base/src/lib/base.spec.ts
@@ -0,0 +1,7 @@
+import { base } from './base';
+
+describe('base', () => {
+  it('should work', () => {
+    expect(base()).toEqual('base');
+  });
+});
---
+++ libs/test/src/lib/test.ts
@@ -0,0 +1,5 @@
+import { base } from 'base';
+
+export function test(): string {
+  return base();
+}
